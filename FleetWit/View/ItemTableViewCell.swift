28a8709894735928467b5c208ca94c8869de5a2e
Ethan Keiser <eak65@drexel.edu>
2019-01-16 03:12:22 -0500

    implemented a customer decoder to handle polymorphic response structure.

---
+++ FleetWit/View/ItemTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  ItemTableViewCell.swift
+//  FleetWit
+//
+//  Created by Ethan Keiser on 1/15/19.
+//  Copyright © 2019 Ethan Keiser. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.commentsLabel.adjustsFontSizeToFitWidth = true;
+        self.timeLabel.adjustsFontSizeToFitWidth = true;
+        self.authorLabel.adjustsFontSizeToFitWidth = true;
+        self.titleLabel.adjustsFontSizeToFitWidth = true;
+        self.titleLabel.minimumScaleFactor = 10/UIFont.labelFontSize
+
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-FleetWit/Pods-FleetWit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FleetWit : NSObject
+@end
+@implementation PodsDummy_Pods_FleetWit
+@end
